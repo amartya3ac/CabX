f2b862a138e609f52f72018f3ecc40f910ccabfb
Amartya <amartyahub03@gmail.com>
2025-03-04 13:41:56 +0530

    Added Source and Destination feature

--- components/Home/InputItem.js
+++ components/Home/InputItem.js
@@ -1,7 +1,30 @@
+"use client";
 import Image from "next/image";
-import React from "react";
+import React, { useEffect, useState } from "react";
+import GooglePlacesAutocomplete from "react-google-places-autocomplete";
 
 function InputItem({ type }) {
+  const [value, setValue] = useState(null);
+  const [placeholder, setPlaceholder] = useState(null);
+
+  useEffect(() => {
+    type == "source"
+      ? setPlaceholder("Pickup Location")
+      : setPlaceholder("Drop Location");
+  }, []);
+
+  const getLatAndLng = (place, type) => {
+    const placeId = place.value.place_id;
+    const service = new google.maps.places.PlacesService(
+      document.createElement("div")
+    );
+    service.getDetails({ placeId }, (place, status) => {
+      if (status == "OK" && place.geometry && place.geometry.location) {
+        console.log(place.geometry.location.lng());
+      }
+    });
+  };
+
   return (
     <div className="bg-slate-200 p-3 rounded-lg mt-3 flex items-center gap-4">
       <Image
@@ -9,11 +32,29 @@ function InputItem({ type }) {
         width={20}
         height={15}
       />
-      <input
-        type="text"
-        placeholder={type == "source" ? "Pickup Location" : "Drop Location"}
-        className="bg-transparent w-full font-semibold 
-      outline-none"
+
+      <GooglePlacesAutocomplete
+        apiKey={process.env.NEXT_PUBLIC_GOOGLE_API_KEY}
+        selectProps={{
+          value,
+          onChange: (place) => {
+            getLatAndLng(place, type);
+            setValue(place);
+          },
+          placeholder: placeholder,
+          isClearable: true,
+          className: "w-full",
+          components: {
+            DropdownIndicator: false,
+          },
+          styles: {
+            control: (provided) => ({
+              ...provided,
+              backgroundColor: "#00ffff00",
+              border: "none",
+            }),
+          },
+        }}
       />
     </div>
   );
